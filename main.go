a5185d644b2ceadc3d1dd6d8a8234f6389c585e6
Javier Uruen Val <javi.uruen@gmail.com>
2018-02-21 19:02:00 +0100

    try to reconnect several times

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 	"github.com/juruen/rmapi/shell"
 )
 
+const AUTH_RETRIES = 3
+
 func main() {
 	log.InitLog()
 
-	ctx := api.CreateApiCtx(api.AuthHttpCtx())
+	var ctx *api.ApiCtx
+	for i := 0; i < AUTH_RETRIES; i++ {
+		ctx = api.CreateApiCtx(api.AuthHttpCtx())
+
+		if ctx.Filetree == nil && i < AUTH_RETRIES {
+			log.Error.Println("retrying...")
+		}
+	}
 
 	if ctx.Filetree == nil {
 		log.Error.Fatal("failed to build documents tree")
