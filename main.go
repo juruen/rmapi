741a3904df693c1ba9478d54059adb1bcde154de
Javier Uruen Val <javi.uruen@gmail.com>
2018-04-05 15:05:17 +0200

    Initial FUSE support for certain operations

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
+	"github.com/hanwen/go-fuse/fuse"
+	"github.com/hanwen/go-fuse/fuse/nodefs"
 	"github.com/juruen/rmapi/api"
+	"github.com/juruen/rmapi/fusefs"
 	"github.com/juruen/rmapi/log"
 	"github.com/juruen/rmapi/shell"
 )
 
 const AUTH_RETRIES = 3
+
+func mount_fuse(ctx *api.ApiCtx) {
+	mountPoint := os.Args[2]
+
+	root := fusefs.NewFuseFsRoot(ctx)
+	conn := nodefs.NewFileSystemConnector(root, nil)
+	server, err := fuse.NewServer(conn.RawFS(), mountPoint, &fuse.MountOptions{})
+	if err != nil {
+		log.Error.Printf("Mount fail: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Info.Println("rM fs mounted on", mountPoint)
+
+	server.Serve()
+}
+
+func run_shell(ctx *api.ApiCtx) {
+	err := shell.RunShell(ctx)
+
+	if err != nil {
+		log.Error.Println("Error: ", err)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	log.InitLog()
@@ -27,10 +54,11 @@ func main() {
 		log.Error.Fatal("failed to build documents tree")
 	}
 
-	err := shell.RunShell(ctx)
-
-	if err != nil {
-		fmt.Println("Error: ", err)
-		os.Exit(1)
+	log.Info.Println(os.Args)
+	if len(os.Args) == 3 && os.Args[1] == "--fuse-mount" {
+		mount_fuse(ctx)
+	} else {
+		run_shell(ctx)
 	}
+
 }
