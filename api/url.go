8300b2e5c1d4c8ac7bb17c9e363a0c41883839b4
Javier Uruen Val <javi.uruen@gmail.com>
2018-09-29 16:24:18 +0200

    update auth endpoints to use new API

--- api/url.go
+++ api/url.go
@@ -1,8 +1,8 @@
 package api
 
 const (
-	newTokenDevice string = "https://my.remarkable.com/token/device/new"
-	newUserDevice  string = "https://my.remarkable.com/token/user/new"
+	newTokenDevice string = "https://my.remarkable.com/token/json/2/device/new"
+	newUserDevice  string = "https://my.remarkable.com/token/json/2/user/new"
 	docHost        string = "https://document-storage-production-dot-remarkable-production.appspot.com"
 	listDocs       string = docHost + "/document-storage/json/2/docs"
 	updateStatus   string = docHost + "/document-storage/json/2/upload/update-status"
