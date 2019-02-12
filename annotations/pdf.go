c748dcdcaff0aa9bea42df638c149b1d64a14226
Loric Brevet <loric.brevet@gmail.com>
2019-02-12 23:08:27 +0100

    Update current code to use new archive and rm packages

--- annotations/pdf.go
+++ annotations/pdf.go
@@ -1,119 +1,74 @@
 package annotations
 
 import (
-	"github.com/jung-kurt/gofpdf"
 	"io/ioutil"
-	"os"
-	"path"
-	"sort"
-	"strconv"
-	"strings"
+
+	"github.com/jung-kurt/gofpdf"
+	"github.com/juruen/rmapi/archive"
+	"github.com/juruen/rmapi/encoding/rm"
 )
 
 const (
-	RmX = 1404
-	RmY = 1872
-	A4X = 210
-	A4Y = 297
+	RmX      = rm.Width
+	RmY      = rm.Height
+	A4X      = 210
+	A4Y      = 297
 	ratioA4X = float32(A4X) / float32(RmX)
 	ratioA4Y = float32(A4Y) / float32(RmY)
-	linesExtension = ".rm"
 )
 
 type PdfGenerator struct {
-	inputDirPath string
+	zipName        string
 	outputFilePath string
 }
 
-type page struct {
-	layers []Layer
-}
-
-func CreatePdfGenerator(inputDirPath, outputFilePath string) PdfGenerator {
-	return PdfGenerator{inputDirPath: inputDirPath, outputFilePath: outputFilePath}
+func CreatePdfGenerator(zipName, outputFilePath string) PdfGenerator {
+	return PdfGenerator{zipName: zipName, outputFilePath: outputFilePath}
 }
 
 func (p PdfGenerator) Generate() error {
-	files, err := ioutil.ReadDir(p.inputDirPath)
+	reader, err := archive.OpenReader(p.zipName)
 	if err != nil {
 		return err
 	}
 
-	pages := make([]page, 0)
-	for _, f := range p.rmFiles(files) {
-		reader, err := CreateRmReader(path.Join(p.inputDirPath, f))
-		if err != nil {
-			return err
-		}
-
-		layers, err := reader.Parse()
-		if err != nil {
-			return err
-		}
-
-		pages = append(pages, page{layers})
-	}
-
-	if err = p.generatePages(pages); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (p PdfGenerator) rmFiles(files []os.FileInfo) []string {
-	rmFiles := make([]string, 0)
-	for _, f := range files {
-		if f.IsDir() || !strings.HasSuffix(f.Name(), linesExtension) {
-			continue
-		}
-
-		pageNumStr := strings.TrimSuffix(f.Name(), linesExtension)
-		_, err := strconv.Atoi(pageNumStr)
-
-		if err != nil {
-			continue
-		}
-
-		rmFiles = append(rmFiles, f.Name())
-	}
-
-	sort.Slice(rmFiles, lineFileSorter(rmFiles))
-
-	return rmFiles
-}
-
-func lineFileSorter(rmFiles []string) func(i int, j int) bool {
-	return func(i int, j int) bool {
-		a, _ := strconv.Atoi(strings.TrimSuffix(rmFiles[i], linesExtension))
-		b, _ := strconv.Atoi(strings.TrimSuffix(rmFiles[j], linesExtension))
-
-		return a < b
-	}
-}
-
-
-func (p PdfGenerator) generatePages(pages []page) error {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 
-	for _, page := range pages {
+	for _, page := range reader.Pages {
+		f, err := page.Data.Open()
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+
+		fb, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		pageRm := rm.New()
+		err = pageRm.UnmarshalBinary(fb)
+		if err != nil {
+			return err
+		}
+
 		pdf.AddPage()
 
-		for _, layer := range page.layers {
-			for _, stroke := range layer.Strokes {
+		for _, layer := range pageRm.Layers {
+			for _, line := range layer.Lines {
 
-				if len(stroke.Segments) < 1 {
+				if len(line.Points) < 1 {
 					continue
 				}
 
-				for i := 1; i < len(stroke.Segments); i++ {
-					s := stroke.Segments[i-1].toA4()
-					x1 := s.X
-					y1 := s.Y
+				for i := 1; i < len(line.Points); i++ {
+					s := line.Points[i-1]
+					x1 := s.X * ratioA4X
+					y1 := s.Y * ratioA4Y
 
-					s = stroke.Segments[i].toA4()
-					x2 := s.X
-					y2 := s.Y
+					s = line.Points[i]
+					x2 := s.X * ratioA4X
+					y2 := s.Y * ratioA4Y
 
 					pdf.Line(float64(x1), float64(y1), float64(x2), float64(y2))
 				}
@@ -122,17 +77,4 @@ func (p PdfGenerator) generatePages(pages []page) error {
 	}
 
 	return pdf.OutputFileAndClose(p.outputFilePath)
-}
-
-func (p PdfGenerator) setStyle(pdf *gofpdf.Fpdf, stroke Stroke) {
-	pdf.SetDrawColor(0, 0,0)
-	pdf.SetLineWidth(0)
-	pdf.SetLineCapStyle("round")
-}
-
-func (s Segment) toA4() Segment {
-	s.X *= ratioA4X
-	s.Y *= ratioA4Y
-
-	return s
 }
--- util/zipdoc.go
+++ util/zipdoc.go
@@ -4,10 +4,7 @@ import (
 	"archive/zip"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/juruen/rmapi/log"
@@ -106,66 +103,6 @@ func CreateZipDirectory(id string) (string, error) {
 	f.Write([]byte("{}"))
 
 	return tmp.Name(), nil
-}
-
-
-func Unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if err := r.Close(); err != nil {
-			panic(err)
-		}
-	}()
-
-	os.MkdirAll(dest, 0755)
-
-	// Closure to address file descriptors issue with all the deferred .Close() methods
-	extractAndWriteFile := func(f *zip.File) error {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer func() {
-			if err := rc.Close(); err != nil {
-				panic(err)
-			}
-		}()
-
-		path := filepath.Join(dest, f.Name)
-
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, 0700)
-		} else {
-			os.MkdirAll(filepath.Dir(path), 0700)
-			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer func() {
-				if err := f.Close(); err != nil {
-					panic(err)
-				}
-			}()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-
-	for _, f := range r.File {
-		err := extractAndWriteFile(f)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func createZipContent(ext string) (string, error) {
