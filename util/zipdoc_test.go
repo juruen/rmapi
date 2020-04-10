62eba0fbefd71c0075ca3d5243e3a00e315dac8a
ddvk <ddvk>
2020-04-10 12:30:50 +0200

    add test pdf file

--- util/zipdoc_test.go
+++ util/zipdoc_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestZipFile(t *testing.T) {
-	d, err := CreateZipDocument("1234", "test.pdf")
+	d, err := CreateZipDocument("1234", "zipdoc_test.pdf")
 	fmt.Println(d)
 	if err != nil {
 		t.Error(err)
