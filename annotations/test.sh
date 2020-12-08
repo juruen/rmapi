7098c402b56c16453d0fe29a031204d94d8fd3cb
f3fora <33097027+f3fora@users.noreply.github.com>
2020-12-08 11:31:54 +0100

    Add an extra testfile

--- annotations/test.sh
+++ annotations/test.sh
@@ -1,7 +1,8 @@
 set -e
 path=$(dirname $0)
 go clean -testcache
-go test -v github.com/juruen/rmapi/annotations 
+go test -v github.com/juruen/rmapi/annotations
+xdg-open /tmp/allPen.pdf
 xdg-open /tmp/strange.pdf
 xdg-open /tmp/tmpl.pdf
 xdg-open /tmp/a3.pdf
